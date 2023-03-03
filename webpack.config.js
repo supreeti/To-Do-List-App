893fe282dd519a5dd3c17e10ef3e24d3b200e423
Supreeti <sksupreetk11@gmail.com>
2023-03-03 19:02:08 +0530

    resolve errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
@@ -7,11 +7,7 @@ module.exports = {
   devServer: {
     static: './dist',
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-  ],
+  
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
