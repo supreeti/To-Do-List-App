511b52a3e8e4c8d3ece6772fe4cf32fe0ed4578f
Supreeti <sksupreetk11@gmail.com>
2023-03-03 19:07:55 +0530

    resolve errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,24 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './dist',
-  }, 
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
-  optimization: {
-    runtimeChunk: 'single',
-  },
+    mode: 'development',
+    entry: './src/index.js',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [{
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+        },],
+    },
+    optimization: {
+        runtimeChunk: 'single',
+    },
 };
